ceef3b5911fd094d8c6cff5acf131cad00ce4557
melquideveloper <melque20@hotmail.com>
2022-07-22 10:31:15 -0300

    'Cafe'

---
+++ ProjetoEntrevista/Data/BancoDeDados.cs
@@ -0,0 +1,19 @@
+﻿using ProjetoEntrevista.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEntrevista.Data
+{
+    public class BancoDeDados : DbContext 
+    {
+        public BancoDeDados(DbContextOptions<BancoDeDados> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ModelContato> Contatos { get; set; }
+    }
+}
