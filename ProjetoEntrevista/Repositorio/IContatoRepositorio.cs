1e9d6f2b6702dd3f7ff3912f795429837ec248a3
melquideveloper <melque20@hotmail.com>
2022-07-24 16:32:46 -0300

    almocando

--- ProjetoEntrevista/Repositorio/IContatoRepositorio.cs
+++ ProjetoEntrevista/Repositorio/IContatoRepositorio.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace ProjetoEntrevista.Repositorio
 {
+
     public interface IContatoRepositorio
     {
         ModelContato Adicionar(ModelContato contato);
+
+        List<ModelContato> BuscarTodos();
     }
 }
